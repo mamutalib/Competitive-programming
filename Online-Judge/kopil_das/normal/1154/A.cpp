e7b11eea8fc9e1d3004965d13f884888a961e74d
mamutalib <mohammadmutalib678@gmail.com>
2022-06-25 01:23:52 +0600

    dublicate file deleted

--- Online-Judge/kopil_das/normal/1154/A.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    long long int x[4];
-    cin>>x[0]>>x[1]>>x[2]>>x[3];
-    sort(x,x+4);
-    cout<<x[3]-x[0]<<" "<<x[3]-x[1]<<" "<<x[3]-x[2]<<endl;
-}
