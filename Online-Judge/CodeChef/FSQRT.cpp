c674b51e8f36c8784d8f805371f09f43db957383
mamutalib <mohammadmutalib678@gmail.com>
2022-06-26 00:37:29 +0600

    initial commit

---
+++ Online-Judge/CodeChef/FSQRT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+
+{
+    int t,n;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+        cout <<floor(sqrt(n))<<endl;
+    }
+}
