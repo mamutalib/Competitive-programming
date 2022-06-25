eb60bdde328c971b77cb3414bae018ed99ac3417
mamutalib <mohammadmutalib678@gmail.com>
2022-06-26 00:33:41 +0600

    initial commit

---
+++ Online-Judge/CodeChef/FLOW002.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,t;
+    cin>>t;
+    while(t--)
+     {
+        cin>>a>>b;
+        cout<<a%b<<endl;
+     }
+
+    return 0;
+
+}
