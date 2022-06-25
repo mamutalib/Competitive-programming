e950361cb0805418ef4183005a8644c2d341a38f
mamutalib <mohammadmutalib678@gmail.com>
2022-06-26 00:07:06 +0600

    template updated

--- template.cpp
+++ template.cpp
@@ -17,20 +17,50 @@ using namespace std;
 #define ull unsigned long long 
 #define ld long double
 #define nl '\n'
-#define fo for(int i = 0; i<n; i++)
+#define fn for(int i = 0; i<n; i++)
 #define ft for(int i = 0; i<t; i++)
 #define wt while(t--)
 #define wn while(n--)
 #define fs first
 #define f first
-#define sc second
-#define S second
+#define s second
 #define pf printf
 #define sc scanf
 
+// void solve(){
+//     int t,n;
+//     cin >> t >> n;
+//     wt{
+//         int pass[n];
+//         fn{
+//             cin >> pass[i];
+//         }
+
+
+//     }
+// }
+void solve() {
+    int t;
+    cin >> t;
+    wt{
+        int n; 
+        cin >> n;
+        while(n!=0){
+            if(n%2!=0){
+                cout << 7;
+                n = n-3;
+            }
+            else {
+                cout << 1 ;
+                n = n-2;
+            }
+        }
+        cout << nl;
+    }
+}
 
 int main() {
-    
+    solve();
 }
 
 
