ce382474375736ffbd13636ed816ef17ee692f09
Md. Abdul Mutalib <mohammadmutalib678@gmail.com>
2023-01-12 07:43:24 +0000

    .

---
+++ LeetCode/reverse-integer.cpp
@@ -0,0 +1,36 @@
+
+/* 
+    Md. Abdul Mutalib
+    B.Sc (Engg.) in CSE
+    North East University Bangladesh
+
+    Connect with me:
+    Github: @mamutalib
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long 
+#define ull unsigned long long 
+#define ld long double
+#define nl '\n'
+#define fn for(int i = 0; i<n; i++)
+#define ft for(int i = 0; i<t; i++)
+#define wt while(t--)
+#define wn while(n--)
+#define fs first
+#define f first
+#define s second
+#define pf printf
+#define sc scanf
+
+
+
+int main() {
+    solve();
+}
+
+
