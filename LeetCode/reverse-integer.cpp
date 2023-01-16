90b176fc4f2d30dd36c53f6341ae3c9894210424
mamutalib <mohammadmutalib678@gmail.com>
2023-01-16 21:52:51 +0600

    reverse-integer added

--- LeetCode/reverse-integer.cpp
+++ LeetCode/reverse-integer.cpp
@@ -13,7 +13,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long 
+using ll = long long ;
 #define ull unsigned long long 
 #define ld long double
 #define nl '\n'
@@ -27,10 +27,36 @@ using ll = long long
 #define pf printf
 #define sc scanf
 
+// int reverse(int x) {
+//     int rev = 0;
+//     while (x != 0) {
+//         int last_digit = x % 10;
+//         x /= 10;
+//         if (rev > INT_MAX/10 || (rev == INT_MAX/10 && last_digit > 7)) return 0;
+//         if (rev < INT_MIN/10 || (rev == INT_MIN/10 && last_digit < -8)) return 0;
+//         rev = rev*10 + last_digit;
+//     }
+//     return rev;
+// }
+
+// This approach is more easier than previous one: 
+int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            if (rev > INT_MAX/10 || rev < INT_MIN/10) return 0;
+            rev = rev*10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
 
 
 int main() {
-    solve();
-}
+    int n;
+    cin >> n;
+    int rev = reverse(n);
+    cout << rev << endl;
 
+    return 0;
+}
 
