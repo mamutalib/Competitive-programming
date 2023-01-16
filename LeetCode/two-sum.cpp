7720acb1525d3a721553fe12930b94746a9f4d68
mamutalib <mohammadmutalib678@gmail.com>
2023-01-16 21:53:08 +0600

    two-sum

---
+++ LeetCode/two-sum.cpp
@@ -0,0 +1,25 @@
+// vector<int> twoSum(vector<int>& nums, int target) {
+//     for (int i = 0; i < nums.size(); i++) {
+//         for (int j = i + 1; j < nums.size(); j++) {
+//             if (nums[i] + nums[j] == target) {
+//                 return { i, j };
+//             }
+//         }
+//     }
+//     return {};
+// }
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return { i, j };
+                }
+            }
+        }
+        return {};
+    }
+
+};
